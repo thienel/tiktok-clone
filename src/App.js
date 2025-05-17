7b0634c4ff5e0c15caee2f35889dabf914b35eb5
thienel <lethien.dev@gmail.com>
2025-05-17 15:34:03 +0700

    implement loading layout mechanism

--- src/App.js
+++ src/App.js
@@ -1,15 +1,35 @@
-import { BrowserRouter as Route, Routes, Router } from 'react-router-dom'
+import { Fragment } from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { publicRoutes } from '~/routes'
+import { DefaultLayout } from '~/components/Layout'
 
 function App() {
   return (
     <Router>
-      <Routes>
-        {publicRoutes.map((route, index) => {
-          const Page = route.component
-          return <Route key={index} path={route.path} element={<Page />} />
-        })}
-      </Routes>
+      <div className="App">
+        <Routes>
+          {publicRoutes.map((route, index) => {
+            var Layout = DefaultLayout
+            if (route.layout) {
+              Layout = route.layout
+            } else if (route.layout === null) {
+              Layout = Fragment
+            }
+            const Page = route.component
+            return (
+              <Route
+                key={index}
+                path={route.path}
+                element={
+                  <Layout>
+                    <Page />
+                  </Layout>
+                }
+              />
+            )
+          })}
+        </Routes>
+      </div>
     </Router>
   )
 }
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,12 @@
 import { default as HomePage } from '~/pages/Home'
 import { default as FollowingPage } from '~/pages/Following'
+import { default as UploadPage } from '~/pages/Upload'
+import { HeaderOnly } from '~/components/Layout'
 
 export const publicRoutes = [
   { path: '/', component: HomePage },
-  { path: '/', component: FollowingPage },
+  { path: '/following', component: FollowingPage },
+  { path: '/upload', component: UploadPage, layout: HeaderOnly },
 ]
 
 export const privateRoutes = []
