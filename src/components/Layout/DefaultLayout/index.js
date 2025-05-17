fc9f2246d21e5975869fbed2852e4802e2626212
thienel <lethien.dev@gmail.com>
2025-05-17 17:06:42 +0700

    modify layout outline

--- src/components/Layout/DefaultLayout/index.js
+++ src/components/Layout/DefaultLayout/index.js
@@ -1,11 +1,15 @@
+import classNames from 'classnames/bind'
+import styles from './DefaultLayout.module.scss'
 import Header from '~/components/Layout/components/Header'
 import Sidebar from './Sidebar'
 
+const ex = classNames.bind(styles)
+
 function DefaultLayout({ children }) {
   return (
-    <div>
+    <div className={ex('wrapper')}>
       <Header />
-      <div classname="container">
+      <div className={ex('container')}>
         <Sidebar />
         <div className="content">{children}</div>
       </div>
