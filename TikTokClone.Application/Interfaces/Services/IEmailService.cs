380c7980e3154071af2d7525dfd03d916a73cadb
thienel <lethien.dev@gmail.com>
2025-06-08 23:39:06 +0700

    feat(email): implement SendPasswordResetEmailAsync method

--- TikTokClone.Application/Interfaces/Services/IEmailService.cs
+++ TikTokClone.Application/Interfaces/Services/IEmailService.cs
@@ -3,6 +3,6 @@ namespace TikTokClone.Application.Interfaces.Services
     public interface IEmailService
     {
         Task<bool> SendEmailVerificationCodeAsync(string email, string verificationCode);
-        Task<bool> SendPasswordResetEmailAsync(string email, string resetToken);
+        Task<bool> SendPasswordResetEmailAsync(string email, string verificationCode, string name);
     }
 }
--- TikTokClone.Infrastructure/Services/EmailServices.cs
+++ TikTokClone.Infrastructure/Services/EmailServices.cs
@@ -54,9 +54,31 @@ namespace TikTokClone.Infrastructure.Services
             }
         }
 
-        public Task<bool> SendPasswordResetEmailAsync(string email, string resetToken)
+        public async Task<bool> SendPasswordResetEmailAsync(string email, string verificationCode, string name)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var subject = $"{verificationCode} is your verification code";
+                var body = GenerateChangePasswordEmailBody(verificationCode, name);
+
+                var mailMessage = new MailMessage
+                {
+                    From = new MailAddress(_configuration["EmailSettings:FromEmail"]!, _configuration["EmailSettings:FromName"]),
+                    Subject = subject,
+                    Body = body,
+                    IsBodyHtml = true
+                };
+
+                mailMessage.To.Add(email);
+                await _smtpClient.SendMailAsync(mailMessage);
+                _logger.LogInformation("Change password email sent successfully to {Email}", email);
+
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
         }
 
         public string GenerateVerificationEmailBody(string code)
@@ -103,6 +125,67 @@ namespace TikTokClone.Infrastructure.Services
                         </div>
                     </div>
                     </body>
+                </html>";
+        }
+        public string GenerateChangePasswordEmailBody(string code, string name)
+        {
+            return $@"
+                <!DOCTYPE html>
+                <html>
+                <head>
+                </head>
+                <body>
+                    <div style='display: flex'>
+                        <div style='width: 100%; max-width: 440px; padding: 0px 20px'>
+                            <div style='width: 100%; padding: 40px 7px'>
+                                <img width='150px' src='https://ci3.googleusercontent.com/meips/ADKq_NYK420UkUAFUsKIvzZlahWkg41X9bA1QHf8TAGuZ6ym8m5XYTWJ2Ee90ro6vYswm83-RT1a0AIsN9w5idF4Z_lvhqDOa86bd0GwD60T3s6Dn0-spVP2AbIsrT452YMV-LIR2a1b2k_SiU-YxQ=s0-d-e1-ft#http://p16-tiktokcdn-com.akamaized.net/obj/tiktok-obj/f70f9d0dc867d6c71ce2cd684a0ffff0' />
+                            </div>
+
+                            <div style='max-width: 100%; background-color: #f1f1f1; padding: 20px 16px; font-weight: bold; font-size: 20px; color: rgb(22,24,35)'>
+                                Change your password
+                            </div>
+
+                            <div style='max-width: 100%; background-color: #f8f8f8; padding: 24px 16px; font-size: 17px; color: rgba(22,24,35,0.75); line-height: 20px'>
+                                <p style='margin-bottom: 20px; color: rgba(22,24,35,0.5)'>
+                                    This is an automatically generated email, please do not reply.
+                                </p>
+                                <p style='margin-bottom: 20px'>Hi {name},</p>
+                                <p style='margin-bottom: 20px'>To change your password, enter this verification code in the TikTok app:</p>
+                                <p style='margin-bottom: 20px; color: rgb(22,24,35); font-weight: bold; font-size: 20px; line-height: 24px'>{code}</p>
+                                <p style='margin-bottom: 20px'>Verification codes expire after 48 hours.</p>
+                                <p style='margin-bottom: 20px'>If you didn't request this change, ignore this message and your password will remain the same.</p>
+                                <p>Best,</p>
+                                <p>TikTok Support Team</p>
+                                <p style='word-break: break-all'>
+                                    TikTok Help Center:
+                                    <a style='color: rgb(0,91,158)' href='https://support.tiktok.com/' target='_blank'>
+                                        https://support.tiktok.com/
+                                    </a>
+                                </p>
+                            </div>
+
+                            <div style='max-width: 100%; padding: 40px 16px 20px; font-size: 15px; color: rgba(22,24,35,0.5); line-height: 18px'>
+                                <div>Have a question?</div>
+                                <div style='margin-bottom: 20px'>
+                                    Check out our help center or contact us in the app using
+                                    <span style='color: rgb(0,91,158); font-weight: bold'>Settings &gt; Report a Problem.</span>
+                                </div>
+                            </div>
+
+                            <div style='border: 0; background-color: rgba(0,0,0,0.12); height: 1px; margin-bottom: 16px'></div>
+
+                            <div style='color: rgba(22,24,35,0.5); margin: 20px 16px 40px 16px; font-size: 12px; line-height: 18px'>
+                                <div>This email was generated for {name}.</div>
+                                <div style='word-break: break-all'>
+                                    <a style='color: rgba(22,24,35,0.5); text-decoration: underline' href='https://www.tiktok.com/en/privacy-policy?lang=en' target='_blank'>
+                                        Privacy Policy
+                                    </a>
+                                </div>
+                                <div>TikTok Clone by Thien Le, FPT University, HCMC</div>
+                            </div>
+                        </div>
+                    </div>
+                </body>
                 </html>";
         }
     }
