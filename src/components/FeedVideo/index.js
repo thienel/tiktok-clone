343fe0f16eba565d3f0a4c29e6a7014bef9b02e1
thienel <lethien.dev@gmail.com>
2025-06-11 18:05:23 +0700

    feat(video): adjust width to fit window while maintaining 9:16 aspect ratio

--- src/components/FeedVideo/index.js
+++ src/components/FeedVideo/index.js
@@ -1,32 +1,27 @@
 import classNames from 'classnames/bind'
 import styles from './FeedVideo.module.scss'
+import { useWindowWidth } from '~/hooks'
 
 const cx = classNames.bind(styles)
 
 function FeedVideo() {
+  const windowWidth = useWindowWidth()
+  const isExpand = windowWidth <= 1024
+
   return (
-    <article className={cx('wrapper')}>
+    <div className={cx('wrapper')}>
       <div className={cx('container')}>
-        <div className={cx('content')}>
+        <article className={cx('content', { expand: isExpand })}>
           <section className={cx('video-wrapper')}>
             <div className={cx('player-container')}>
               <video className={cx('video')} src="test-video.mp4" controls autoPlay loop muted />
-
-              {/* <video
-                  class={cx('video')}
-                  x5-playsinline="true"
-                  webkit-playsinline="true"
-                  mediatype="video"
-                  data-index="-1"
-                  preload="auto"
-                  src="test-video-mp4"
-                /> */}
             </div>
           </section>
-        </div>
+          <section className={cx('section-actionBar')}></section>
+        </article>
       </div>
       <aside className={cx('actionBar')}></aside>
-    </article>
+    </div>
   )
 }
 
