3a13b30e3fcb87f10b9733d498fe4ca5d6ef4dc7
thienel <lethien.dev@gmail.com>
2025-06-11 14:52:26 +0700

    feat(sidebar): show required tooltips after user logs in

--- src/components/Sidebar/MainNav/index.js
+++ src/components/Sidebar/MainNav/index.js
@@ -2,17 +2,20 @@ import classNames from 'classnames/bind'
 import { tooltipItems, tooltips } from '~/constants/tooltip'
 import styles from './MainNav.module.scss'
 import Button from '~/components/Button'
+import { useAuth } from '~/hooks'
 
 const cx = classNames.bind(styles)
 
 function MainNav({ onSelect, selectedTooltip, isCollapsed }) {
+  const { isAuthenticated } = useAuth()
+
   return (
     <div className={cx('MainNavWrapper', { isCollapsed })}>
-      {tooltipItems.map(({ key, label, size, focused, url }) => {
+      {tooltipItems.map(({ key, label, size, focused, url, loginRequired }) => {
         const Icon = tooltips[key]
         const IconSelected = tooltips[focused]
         const isSelected = selectedTooltip === key
-        return (
+        return (!loginRequired || (loginRequired && isAuthenticated)) && (
           <Button key={key} to={url} onClick={() => onSelect(key)} selected={isSelected} left>
             <div className={cx('TooltipContent')}>
               <div className={cx('TooltipIconWrapper')} style={{ fontSize: size }}>
@@ -21,7 +24,7 @@ function MainNav({ onSelect, selectedTooltip, isCollapsed }) {
               <span>{label}</span>
             </div>
           </Button>
-        )
+              )
       })}
     </div>
   )
--- src/constants/tooltip.js
+++ src/constants/tooltip.js
@@ -1,13 +1,16 @@
 const tooltipImages = require.context('!!@svgr/webpack!../assets/images/tooltip', false, /\.svg$/)
 
 const tooltipItems = [
-  { key: 'foryou', label: 'For You', size: 32, focused: 'foryouFocused', url: '/' },
-  { key: 'explore', label: 'Explore', size: 32, focused: 'exploreFocused', url: '/' },
-  { key: 'following', label: 'Following', size: 24, focused: 'followingFocused', url: '/' },
-  { key: 'upload', label: 'Upload', size: 24, focused: 'upload', url: '/' },
-  { key: 'live', label: 'LIVE', size: 32, focused: 'liveFocused', url: '/' },
-  { key: 'profile', label: 'Profile', size: 24, focused: 'profile', url: '/' },
-  { key: 'more', label: 'More', size: 24, focused: 'more', url: '/' },
+  { key: 'foryou', label: 'For You', size: 32, focused: 'foryouFocused', url: '/' , loginRequired: false},
+  { key: 'explore', label: 'Explore', size: 32, focused: 'exploreFocused', url: '/', loginRequired: false },
+  { key: 'following', label: 'Following', size: 24, focused: 'followingFocused', url: '/' , loginRequired: false},
+  { key: 'friends', label: 'Friends', size: 32, focused: 'friendsFocused', url: '/', loginRequired: true},
+  { key: 'upload', label: 'Upload', size: 24, focused: 'upload', url: '/' , loginRequired: false},
+  { key: 'activity', label: 'Activity', size: 32, focused: 'activityFocused', url: '/', loginRequired: true },
+  { key: 'messages', label: 'Messages', size: 24, focused: 'messagesFocused', url: '/', loginRequired: true},
+  { key: 'live', label: 'LIVE', size: 32, focused: 'liveFocused', url: '/' , loginRequired: false},
+  { key: 'profile', label: 'Profile', size: 24, focused: 'profile', url: '/' , loginRequired: false},
+  { key: 'more', label: 'More', size: 24, focused: 'more', url: '/' , loginRequired: false},
 ]
 
 const tooltips = {}
