d4440485d7354274cc7486a629a8422e69ed439c
thienel <lethien.dev@gmail.com>
2025-06-12 14:16:26 +0700

    chore(auth,user): add dtos anootations & init base method in user service interface

--- TikTokClone.Application/Interfaces/Services/IUserService.cs
+++ TikTokClone.Application/Interfaces/Services/IUserService.cs
@@ -1,8 +1,14 @@
-
+using TikTokClone.Application.DTOs;
+using TikTokClone.Domain.Entities;
 
 namespace TikTokClone.Application.Interfaces.Services
 {
     public interface IUserService
     {
+        Task<ProfileResponseDto> GetProfileAsync(string userId);
+        Task<ProfileResponseDto> UpdateProfileAsync(User user);
+        Task ChangeAvatarAsync(string userId, string avatarURL);
+        // Task GetNotifications();
+        Task Search(string value);
     }
 }
--- TikTokClone.Application/Services/UserService.cs
+++ TikTokClone.Application/Services/UserService.cs
@@ -1,6 +1,8 @@
 
+using TikTokClone.Application.DTOs;
 using TikTokClone.Application.Interfaces.Repositories;
 using TikTokClone.Application.Interfaces.Services;
+using TikTokClone.Domain.Entities;
 
 namespace TikTokClone.Application.Services
 {
@@ -11,6 +13,26 @@ namespace TikTokClone.Application.Services
         public UserService(IUserRepository userRepository)
         {
             _userRepository = userRepository;
+        }
+
+        public Task ChangeAvatarAsync(string userId, string avatarURL)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ProfileResponseDto> GetProfileAsync(string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Search(string value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ProfileResponseDto> UpdateProfileAsync(User user)
+        {
+            throw new NotImplementedException();
         }
     }
 }
