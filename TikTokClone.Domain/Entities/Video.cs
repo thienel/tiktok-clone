9166c461084ca18ef06414b29d717cc8f5eb5dbb
thienel <lethien.dev@gmail.com>
2025-06-14 20:52:50 +0700

    feat(Video): add basic validation

--- TikTokClone.Domain/Entities/Video.cs
+++ TikTokClone.Domain/Entities/Video.cs
@@ -13,8 +13,27 @@ namespace TikTokClone.Domain.Entities
         public DateTime UpdatedAt { get; private set; }
         virtual public User User { get; private set; } = null!;
 
+        public const int MaxDescriptionLength = 4000;
+        public const int MaxTitleLength = 100;
+
         public Video(string title, string description, string url, string thumbnailUrl, string userId)
         {
+            if (!IsValidDescription(description))
+            {
+                throw new Exception();
+            }
+            if (!IsValidTitle(title))
+            {
+                throw new Exception();
+            }
+            if (!IsValidUrl(url))
+            {
+                throw new Exception();
+            }
+            if (!IsValidUrl(thumbnailUrl))
+            {
+                throw new Exception();
+            }
             Title = title;
             Description = description;
             Url = url;
@@ -31,6 +50,21 @@ namespace TikTokClone.Domain.Entities
                 return;
             IsVisible = isVisible;
             UpdatedAt = DateTime.UtcNow;
+        }
+
+        private bool IsValidTitle(string title)
+        {
+            return title.Trim().Length <= MaxTitleLength;
+        }
+
+        private bool IsValidDescription(string description)
+        {
+            return description.Trim().Length <= MaxDescriptionLength;
+        }
+
+        private bool IsValidUrl(string url)
+        {
+            return Uri.TryCreate(url, UriKind.Absolute, out _);
         }
     }
 }
