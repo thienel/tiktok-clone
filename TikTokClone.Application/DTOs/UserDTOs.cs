383ac87ca35e20fa84fd2c7a7038e5aa7215c728
thienel <lethien.dev@gmail.com>
2025-06-15 21:27:12 +0700

    feat(User): add and update methods in IUserService

--- TikTokClone.Application/DTOs/UserDTOs.cs
+++ TikTokClone.Application/DTOs/UserDTOs.cs
@@ -9,11 +9,16 @@ namespace TikTokClone.Application.DTOs
         public string? Bio { get; set; }
     }
 
-    public class UserResponseDto<T> where T : class
+    public class UserResponseDto
     {
-        public string IsSuccess { get; set; }
-        public string Message { get; set; }
-        public string ErrorCode { get; set; }
-        public T Data { get; set; }
+        public bool IsSuccess { get; set; }
+        public string Message { get; set; } = string.Empty;
+        public string ErrorCode { get; set; } = string.Empty;
+        public ProfileResponseDto? Profile { get; set; } = null;
+    }
+
+    public class SearchUserResponseDto
+    {
+        List<ProfileResponseDto> Users { get; set; } = new List<ProfileResponseDto>();
     }
 }
--- TikTokClone.Application/Interfaces/Services/IUserService.cs
+++ TikTokClone.Application/Interfaces/Services/IUserService.cs
@@ -5,10 +5,13 @@ namespace TikTokClone.Application.Interfaces.Services
 {
     public interface IUserService
     {
-        Task<ProfileResponseDto> GetProfileAsync(string userId);
-        Task<ProfileResponseDto> UpdateProfileAsync(User user);
-        Task ChangeAvatarAsync(string userId, string avatarURL);
-        // Task GetNotifications();
-        Task Search(string value);
+        Task<UserResponseDto> GetProfileAsync(string userId);
+        Task<UserResponseDto> ChangeAvatarAsync(string userId, string avatarURL);
+        Task<UserResponseDto> ChangeBioAsync(string userId, string bio);
+        Task<UserResponseDto> ChangeNameAsync(string userId, string name);
+        Task<UserResponseDto> ChangeUserNameAsync(string userId, string userName);
+        Task<UserResponseDto> VerifyUserAsync(string userId);
+        Task<UserResponseDto> UnVerifyUserAsync(string userId);
+        Task<SearchUserResponseDto> Search(string value);
     }
 }
