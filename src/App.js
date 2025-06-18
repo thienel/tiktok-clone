f2e568c8695f3063653419424a5a7e957baa3b71
thienel <lethien.dev@gmail.com>
2025-06-18 14:58:55 +0700

    feat(context): create loading context

--- src/App.js
+++ src/App.js
@@ -2,20 +2,23 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { publicRoutes } from '~/routes'
 import ErrorBoundary from '~/components/ErrorBoundary'
 import { AuthProvider } from '~/context/AuthContext'
+import { LoadingProvider } from '~/context/LoadingContext'
 
 function App() {
   return (
     <ErrorBoundary>
-      <AuthProvider>
-        <Router>
-          <Routes>
-            {publicRoutes.map((route, index) => {
-              const Page = route.component
-              return <Route key={index} path={route.path} element={<Page />} />
-            })}
-          </Routes>
-        </Router>
-      </AuthProvider>
+      <LoadingProvider>
+        <AuthProvider>
+          <Router>
+            <Routes>
+              {publicRoutes.map((route, index) => {
+                const Page = route.component
+                return <Route key={index} path={route.path} element={<Page />} />
+              })}
+            </Routes>
+          </Router>
+        </AuthProvider>
+      </LoadingProvider>
     </ErrorBoundary>
   )
 }
