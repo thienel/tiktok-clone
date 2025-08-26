524b91d9f69682ceb33185e49f789097a6ab1953
thienel <lethien.dev@gmail.com>
2025-08-26 18:32:17 +0700

    chore: remove duplicate directories after repository merge

--- auth-service/TikTokClone.Application/Interfaces/Repositories/IEmailCodeRepository.cs
+++
@@ -1,9 +0,0 @@
-using TikTokClone.Domain.Entities;
-
-namespace TikTokClone.Application.Interfaces.Repositories
-{
-    public interface IEmailCodeRepository : IBaseRepository<EmailCode>
-    {
-        Task<EmailCode?> FindByEmailAsync(string email);
-    }
-}
