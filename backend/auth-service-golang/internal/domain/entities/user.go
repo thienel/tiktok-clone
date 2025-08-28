9b887b04f0b88b9cba31689589a1174989e10bf1
thienel <lethien.dev@gmail.com>
2025-08-28 16:57:54 +0700

    [auth-service] refactor(entities): change user schema

--- backend/auth-service-golang/internal/domain/entities/user.go
+++ backend/auth-service-golang/internal/domain/entities/user.go
@@ -8,17 +8,10 @@ import (
 
 type User struct {
 	ID        uuid.UUID `json:"id" db:"id"`
+	UserName  string    `json:"user_name" db:"user_name"`
 	Email     string    `json:"email" db:"email"`
 	Password  string    `json:"-" db:"password_hash"`
-	FirstName string    `json:"first_name" db:"first_name"`
-	LastName  string    `json:"last_name" db:"last_name"`
 	Status    string    `json:"status" db:"status"`
 	CreatedAt time.Time `json:"created_at" db:"created_at"`
 	UpdatedAt time.Time `json:"updated_at" db:"updated_at"`
 }
-
-const (
-	UserStatusActive   = "active"
-	UserStatusInactive = "inactive"
-	UserStatusPending  = "pending"
-)
