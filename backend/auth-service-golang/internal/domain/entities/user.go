c6cfe9000a5ddc64581285bbbaf011121f9f643c
thienel <lethien.dev@gmail.com>
2025-08-29 19:07:44 +0700

    [auth-service] refactor(entities): modify user entity for better consistency and supporting for gorm

--- backend/auth-service-golang/internal/domain/entities/user.go
+++ backend/auth-service-golang/internal/domain/entities/user.go
@@ -7,6 +7,7 @@ import (
 	"time"
 
 	"github.com/google/uuid"
+	"gorm.io/gorm"
 )
 
 type UserStatus string
@@ -16,32 +17,31 @@ const (
 	UserStatusInactive  UserStatus = "inactive"
 	UserStatusSuspended UserStatus = "suspended"
 	UserStatusPending   UserStatus = "pending"
-	UserStatusDeleted   UserStatus = "deleted"
 )
 
 type User struct {
-	ID           uuid.UUID  `json:"id" db:"id" validate:"required"`
-	UserName     string     `json:"user_name" db:"user_name" validate:"required,min=2,max=24"`
-	Email        string     `json:"email" db:"email" validate:"required,email"`
-	PasswordHash string     `json:"-" db:"password_hash" validate:"required"`
-	Status       UserStatus `json:"status" db:"status" validate:"required,oneof=active inactive suspended pending deleted"`
-	CreatedAt    time.Time  `json:"created_at" db:"created_at"`
-	UpdatedAt    time.Time  `json:"updated_at" db:"updated_at"`
-	DeletedAt    *time.Time `json:"deleted_at" db:"deleted_at"`
-	Tokens       []Token    `json:"tokens,omitempty" db:"-"`
+	ID           uuid.UUID      `json:"id" validate:"required" gorm:"primaryKey"`
+	Username     string         `json:"username" validate:"required,min=2,max=24"`
+	Email        string         `json:"email" validate:"required,email"`
+	PasswordHash string         `json:"-" validate:"required"`
+	Status       UserStatus     `json:"status" validate:"required,oneof=active inactive suspended pending"`
+	CreatedAt    time.Time      `json:"created_at" gorm:"autoCreateTime"`
+	UpdatedAt    time.Time      `json:"updated_at" gorm:"autoUpdateTime"`
+	DeletedAt    gorm.DeletedAt `json:"deleted_at" gorm:"index"`
+	Tokens       []Token        `json:"tokens,omitempty" gorm:"foreignKey:UserID;constraint:OnDelete:CASCADE"`
 }
 
 type PublicUser struct {
 	ID        uuid.UUID  `json:"id"  validate:"required"`
-	UserName  string     `json:"user_name"  validate:"required,min=2,max=24"`
+	Username  string     `json:"username"  validate:"required,min=2,max=24"`
 	Email     string     `json:"email"  validate:"required,email"`
-	Status    UserStatus `json:"status"  validate:"required,oneof=active inactive suspended pending deleted"`
+	Status    UserStatus `json:"status"  validate:"required,oneof=active inactive suspended pending"`
 	CreatedAt time.Time  `json:"created_at"`
 	UpdatedAt time.Time  `json:"updated_at"`
 }
 
 type UserCreateRequest struct {
-	UserName string `json:"user_name"  validate:"required,min=3,max=24"`
+	Username string `json:"username"  validate:"required,min=2,max=24"`
 	Email    string `json:"email"  validate:"required,email"`
 	Password string `json:"password"  validate:"required,min=8"`
 }
@@ -50,32 +50,24 @@ func NewUser(request UserCreateRequest, hashedPassword string) *User {
 	now := time.Now()
 	return &User{
 		ID:           uuid.New(),
-		UserName:     request.UserName,
+		Username:     request.Username,
 		Email:        strings.ToLower(request.Email),
 		PasswordHash: hashedPassword,
 		Status:       UserStatusPending,
 		CreatedAt:    now,
 		UpdatedAt:    now,
-		DeletedAt:    nil,
 	}
 }
 
-func (user *User) ToPublicUser() PublicUser {
-	return PublicUser{
+func (user *User) ToPublicUser() *PublicUser {
+	return &PublicUser{
 		ID:        user.ID,
-		UserName:  user.UserName,
+		Username:  user.Username,
 		Email:     user.Email,
 		Status:    user.Status,
 		CreatedAt: user.CreatedAt,
 		UpdatedAt: user.UpdatedAt,
 	}
-}
-
-func (user *User) SoftDelete() {
-	now := time.Now()
-	user.DeletedAt = &now
-	user.Status = UserStatusDeleted
-	user.UpdatedAt = now
 }
 
 func (us UserStatus) Value() (driver.Value, error) {
