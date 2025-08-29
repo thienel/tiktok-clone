06330b5328e4de15d010bd4ad85fca5914eb4151
thienel <lethien.dev@gmail.com>
2025-08-29 19:09:08 +0700

    [auth-service] refactor(migrations): modify create users table to associated with user entity

--- backend/auth-service-golang/internal/infrastructure/database/migrations/000001_create_users_table.up.sql
+++ backend/auth-service-golang/internal/infrastructure/database/migrations/000001_create_users_table.up.sql
@@ -1,16 +1,18 @@
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
+CREATE TYPE user_status AS ENUM ('active', 'inactive', 'suspended', 'pending');
+
 CREATE TABLE users (
     id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
-    user_name VARCHAR(50) UNIQUE NOT NULL,
+    username VARCHAR(50) UNIQUE NOT NULL,
     email VARCHAR(100) UNIQUE NOT NULL,
     password_hash VARCHAR(255) NOT NULL,
-    status VARCHAR(20) NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    deleted_at TIMESTAMP DEFAULT NULL
+    status user_status NOT NULL DEFAULT 'pending',
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    deleted_at TIMESTAMP WITH TIME ZONE
 );
 
-CREATE INDEX idx_users_user_name ON users(user_name);
+CREATE INDEX idx_users_username ON users(username);
 CREATE INDEX idx_users_email ON users(email);
-
+CREATE INDEX idx_users_deleted_at ON users(deleted_at);
