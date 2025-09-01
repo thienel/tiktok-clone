1bc3f9d22d6e37e820f23311c8819f4c913fafda
thienel <lethien.dev@gmail.com>
2025-09-01 20:35:35 +0700

    [auth-service] refactor(migrations): modify migrations to match with domain entities

--- backend/auth-service-golang/internal/infrastructure/database/migrations/000002_create_tokens_table.up.sql
+++ backend/auth-service-golang/internal/infrastructure/database/migrations/000002_create_tokens_table.up.sql
@@ -1,15 +1,18 @@
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
+CREATE TYPE token_type AS ENUM ('access', 'refresh');
+
 CREATE TABLE tokens (
-    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
-    user_id UUID REFERENCES users(id) ON DELETE CASCADE,
-    token VARCHAR(255) UNIQUE NOT NULL,
-    type VARCHAR(20) NOT NULL,
-    expiry_at TIMESTAMP NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    revoked_at TIMESTAMP DEFAULT NULL,
-    deleted_at TIMESTAMP DEFAULT NULL
+    id UUID PRIMARY KEY,
+    user_id UUID NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+    token TEXT NOT NULL,
+    type token_type NOT NULL,
+    expiry_at TIMESTAMP WITH TIME ZONE NOT NULL,
+    created_at TIMESTAMP WITH TIME ZONE NOT NULL,
+    deleted_at TIMESTAMP WITH TIME ZONE DEFAULT NULL
 );
 
 CREATE INDEX idx_tokens_user_id ON tokens(user_id);
 CREATE INDEX idx_tokens_token ON tokens(token);
+CREATE INDEX idx_tokens_deleted_at ON tokens(deleted_at);
+CREATE INDEX idx_tokens_user_type_active ON tokens(user_id, type) WHERE deleted_at IS NULL;
