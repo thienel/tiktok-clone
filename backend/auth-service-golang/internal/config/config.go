dc050f222d58ea2b5ad785c8d5c729b822250729
thienel <lethien.dev@gmail.com>
2025-09-03 15:04:59 +0700

    [auth-service] feat(config): add PublicKeyPath and PrivateKeyPath

--- backend/auth-service-golang/internal/config/config.go
+++ backend/auth-service-golang/internal/config/config.go
@@ -5,20 +5,22 @@ import (
 )
 
 type Config struct {
-	Port        string
-	DatabaseURL string
-	JWTSecret   string
-	LogLevel    string
-	RedisURL    string
+	Port           string
+	DatabaseURL    string
+	LogLevel       string
+	RedisURL       string
+	PublicKeyPath  string
+	PrivateKeyPath string
 }
 
 func Load() *Config {
 	return &Config{
-		Port:        getEnv("PORT", "8080"),
-		DatabaseURL: getEnv("DATABASE_URL", "postgres://auth_service:password@localhost:5432/auth_service?sslmode=disable"),
-		JWTSecret:   getEnv("JWT_SECRET", "your-secret-key"),
-		LogLevel:    getEnv("LOG_LEVEL", "info"),
-		RedisURL:    getEnv("REDIS_URL", "redis://localhost:6379"),
+		Port:           getEnv("PORT", "8080"),
+		DatabaseURL:    getEnv("DATABASE_URL", "postgres://auth_service:password@localhost:5432/auth_service?sslmode=disable"),
+		LogLevel:       getEnv("LOG_LEVEL", "info"),
+		RedisURL:       getEnv("REDIS_URL", "redis://localhost:6379"),
+		PublicKeyPath:  getEnv("PUBLIC_KEY_PATH", "./keys/public.pem"),
+		PrivateKeyPath: getEnv("PRIVATE_KEY_PATH", "./keys/private.pem"),
 	}
 }
 
